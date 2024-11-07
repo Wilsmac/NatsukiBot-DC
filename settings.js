337a99c1f4a49e6883cb2c4e5298785ef9ad292b
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-11-06 22:16:19 -0600

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -4,9 +4,7 @@ const chalk = require('chalk');
 
 //---------[ PROPIETARIO/OWNER ]---------
 global.owner = [
-["1008834879858946170"],
-["750524319876120657"],
-["1132831529190559744"], //Aca pueden poner tu ID para que sea owner del bot
+["1008834879858946170"] //Aca pueden poner tu ID para que sea owner del bot
 ]
 
 //---------[ Token ]---------
@@ -75,16 +73,15 @@ global.img2 = 'https://qu.ax/jzhN.jpg' //Cambiar la url por tu imagen o guarda l
 global.imagen1 = fs.readFileSync('./media/menu.jpg')
  
 //---------[ ENLACES ]---------
-global.md = 'https://github.com/elrebelde21'
-global.yt = 'https://www.youtube.com/@elrebelde.21'
-global.tiktok = 'tiktok.com/@elrebelde21'
-global.fb = 'https://www.facebook.com/elrebelde21'
-global.faceb = 'https://facebook.com/groups/872989990425789/'
-global.paypal = 'https://paypal.me/OficialGD' 
-//pueden cambiar los links por los tuyo como gusten :v
+global.md = 'https://github.com/Wilsmac'
+global.yt = 'https://www.youtube.com/'
+global.tiktok = 'tiktok.com/'
+global.fb = 'https://www.facebook.com/'
+global.faceb = 'https://facebook.com/'
+global.paypal = 'https://paypal.me/' 
 
-global.nna = "https://whatsapp.com/channel/0029Va4QjH7DeON0ePwzjS1A" //Canal de WhatsApp "Infinity-wa"
-global.nn = "https://discord.gg/zvKgtc2RBc" //Servidor de discord "SkyUltraPlus" 
+global.nna = "https://whatsapp.com"
+global.nn = "https://discord.gg/" 
 
 //---------[ INFO ]--------- 
 global.info = {
